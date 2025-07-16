95eec807115ae665c6cdf3102c69c59635805e97
Siyana Trendafilova <bebovska@gmail.com>
2025-07-16 21:09:20 +0300

    feat: Add registration functionality

--- userModelAndPasswordManagement/forumApp/accounts/models.py
+++ userModelAndPasswordManagement/forumApp/accounts/models.py
@@ -2,4 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    points = models.IntegerField()
+    points = models.IntegerField(
+        null=True,
+        blank=True,
+    )

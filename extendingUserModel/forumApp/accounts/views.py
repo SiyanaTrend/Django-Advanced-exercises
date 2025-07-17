0bbabec1f4455ba5776ffd439c36af3928278198
Siyana Trendafilova <bebovska@gmail.com>
2025-07-17 18:05:17 +0300

    feat: Add signal to create profile when user is created

--- extendingUserModel/forumApp/accounts/views.py
+++ extendingUserModel/forumApp/accounts/views.py
@@ -6,3 +6,4 @@ class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('dashboard')
+    # Uses signal to create the profile

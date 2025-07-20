5bf64e45b62b053511f6948ac66a9148d20b4a3c
Siyana Trendafilova <bebovska@gmail.com>
2025-07-20 15:08:13 +0300

    Initial commit

---
+++ djangoMiddlewareSessionsCookies/forumApp/posts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from posts import views
+from posts.views import MyRedirectView
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
+    path('post/', include([
+        path('add/', views.CreatePost.as_view(), name='add-post'),
+        path('approve/<int:pk>/', views.approve_post, name='approve-post'),
+        path('edit/<int:pk>/', views.EditPost.as_view(), name='edit-post'),
+        path('delete/<int:pk>/', views.DeletePost.as_view(), name='delete-post'),
+        path('details/<int:pk>/', views.PostDetails.as_view(), name='post-details'),
+    ])),
+    path('redirect/', MyRedirectView.as_view())
+]
+
